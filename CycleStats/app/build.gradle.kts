0bf338af7c752f90ee6eb4e21c2889eb99b9a7ac
Keaton Brinkerhoff <keatonbrink@gmail.com>
2024-02-11 14:23:27 -0700

    Add list item xml

--- CycleStats/app/build.gradle.kts
+++ CycleStats/app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
 
     implementation("androidx.fragment:fragment-ktx:1.6.2")
 
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- CycleStats/app/src/main/java/com/keatonbrink/android/cyclestats/TripListFragment.kt
+++ CycleStats/app/src/main/java/com/keatonbrink/android/cyclestats/TripListFragment.kt
@@ -2,16 +2,44 @@ package com.keatonbrink.android.cyclestats
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.keatonbrink.android.cyclestats.databinding.FragmentTripListBinding
 
 private const val TAG = "TripListFragment"
 
 class TripListFragment: Fragment() {
+    private var _binding: FragmentTripListBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
     private val tripListViewModel: TripListViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "Total trips: ${tripListViewModel.getTrips().size}")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTripListBinding.inflate(inflater, container, false)
+
+        binding.tripsRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
